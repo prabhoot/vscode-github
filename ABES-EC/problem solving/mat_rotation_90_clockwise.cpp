df7f6eba3909c8cc1fb222a05bc31421e6e5be6c
prabhu <93818934+prabhoot@users.noreply.github.com>
2022-11-09 20:04:04 +0000

    Update mat_rotation_90_clockwise.cpp

--- ABES-EC/problem solving/mat_rotation_90_clockwise.cpp
+++ ABES-EC/problem solving/mat_rotation_90_clockwise.cpp
@@ -27,28 +27,29 @@ for(int i=0;i<n;i++){
 	for(int j=0;j<n;j++){
 		a[i][j]=b[i][j];
 	}
-}
+}}
 void interchange_rows(int a[][n]){
+    	int b[n][n];
    for(int i=0;i<n;i++)
     {
         for(int j=0;j<n;j++){
         b[i][j]=a[n-i-1][j];}}
-}}
 
 for(int i=0;i<n;i++){
 	for(int j=0;j<n;j++){
 		a[i][j]=b[i][j];
 	}
 }
+}
 int main(){
 	int A[n][n] = { { 1,2,3},{4,5,6},{7,8,9}};
 	transpose_mat(A);
-	interchange_rows(a);
+	interchange_rows(A);
     // interchange_col(B,A);
 	cout << "Result matrix is "<<endl;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++)
-			cout << " " << B[i][j];
+			cout << " " << A[i][j];
 		cout <<endl;
 	}
 	return 0;
