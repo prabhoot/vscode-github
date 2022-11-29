d09dfe5e688465d41855ed3d4a060a960fff6a3f
Prabhoot Patel <prabhootpatel@gmail.com>
2022-11-29 19:40:07 +0530

    files uplode

--- ABES-EC/data structure/tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-#include <bits/stdc++.h> // header file includes every Standard library
-using namespace std;
-int sum_of_digits(int n) {
-  int c = n;
-  int a = c / 10;
-  int b = n - a * 10;
-  return a + b;
