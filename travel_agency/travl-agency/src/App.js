a86ed0cafde5b66192568bba188645de5fb13201
Prabhoot Patel <prabhootpatel@gmail.com>
2023-05-09 14:57:14 +0530

    messages

--- travel_agency/travl-agency/src/App.js
+++ travel_agency/travl-agency/src/App.js
@@ -7,10 +7,7 @@ import Home from "./components/Home";
 const App = () => {
   return (
     <>
-      <Home/>
-      <Switch>
         <Route path="/" Component={Home}/>
-      </Switch>
     </>
   );
 };
