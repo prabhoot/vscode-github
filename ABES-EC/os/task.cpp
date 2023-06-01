0ed0db121504190df75c741fc281cb11a4861f95
prabhoot <93818934+prabhoot@users.noreply.github.com>
2023-06-01 18:10:59 +0530

    done

--- ABES-EC/os/task.cpp
+++ ABES-EC/os/task.cpp
@@ -7,10 +7,6 @@ vector<int> at, bt, pid, ct, tat, wat, rt, gc;
 vector<pair<int, pair<int, int>>> v;
 //           at       pid  bt
 void print_id(int n) {
-    // if(n==-1]){
-    //     return;
-    // }
-    // cout<<"the value "<<n<<" ";
     cout << setw(n / 2);
     cout << " P" << v[t].s.f << " ";
     cout << setw(n / 2);
@@ -46,14 +42,12 @@ int main() {
         gc.push_back(0);
     }
     for (int i = 0; i < n; i++) {
-        // cout << current_time << " ";
         auto arrival_time = v[i].f;
         auto process_id = v[i].s.f;
         auto burst_time = v[i].s.s;
         rt.push_back(current_time - arrival_time);
         ct.push_back(current_time + burst_time);
         current_time = max(ct[i], v[i + 1].f);
-        // cout << "the currrent time is" << current_time <<" "<<ct[i]<<" "<< v[i + 1].f<< endl;
         tat.push_back(ct[i] - arrival_time);
         wat.push_back(tat[i] - burst_time);
 
@@ -66,7 +60,6 @@ int main() {
             gc.push_back(ct[i]);
         }
     }
-
     cout << "Tabular form->\n";  // Tabular form :
     cout << setw(10) << "at" << setw(10) << "pid" << setw(10) << "bt" << setw(10) << "ct" << setw(10) << "tat" << setw(10) << "wat" << setw(10) << "rt" << endl;
     for (int i = 0; i < n; i++) {
@@ -75,14 +68,13 @@ int main() {
         auto c = v[i].s.s;
         cout << setw(10) << a << setw(10) << b << setw(10) << c << setw(10) << ct[i] << setw(10) << tat[i] << setw(10) << wat[i] << setw(10) << rt[i] << endl;
     }
-    cout << "\n\n";
+    cout << "\n";
     cout << "Gantt chart->\n";  // Gantt chart :
-    // cout << setw(6);
     for (int i = 0; i < gc.size(); i++) {
-        // print_bar(gc[i]);  print_bar(gc[i + 1] - gc[i - 1]);
         if (gc[i + 1] == -1) {
             cout << gc[i];
             print_bar(gc[i + 2] - gc[i]);
+
             i++;
         } else {
             cout << gc[i];
@@ -91,10 +83,22 @@ int main() {
             }
         }
     }
-    cout << endl;
-    for (auto i : gc) {
-        cout << i << " ";
+    double sum=0;
+    double size=tat.size();
+    cout<<endl;
+    cout<<endl;
+    for(auto i:tat){
+        sum+=i;
     }
+    cout<<"The Average turn around time is: "<<sum/size;
+    sum=0;
+    for(auto i:wat){
+        sum+=i;
+    }
+    cout<<endl;
+    cout<<"The Average Waiting time is: "<<sum/size;
+    cout << endl;
+
     return 0;
 }
 // Input:
@@ -107,3 +111,7 @@ int main() {
 // 1 2 5
 // 2 3 3
 // 3 4 9
+// 3
+// 0 0 10
+// 1 10 5
+// 2 15 8
